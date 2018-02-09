cc14fa1132e680568860f4c04d3c2110fb1b7ca8
Josiah Laivins <jokellum@northstate.net>
2018-02-09 17:55:15 -0500

    Fixed the 'bug' in the adder, being that I would multiply instead of divide. Will cause Travis to pass tests now

--- unit_tests/test_ObjectForTesting.py
+++ unit_tests/test_ObjectForTesting.py
@@ -7,8 +7,8 @@ class test_ObjectForTesting(unittest.TestCase):
         from ObjectForTesting import ObjectForTesting
         m = ObjectForTesting()
         k = m.adder(1, 1)
-        self.assertEqual(k, 1, 'The numbers are not multiplying properly')
+        self.assertEqual(k, 2, 'The numbers are not adding properly')
         k = m.adder(1, 2)
-        self.assertEqual(k, 3, 'The numbers are not multiplying properly')
+        self.assertEqual(k, 3, 'The numbers are not adding properly')
         k = m.adder(2, 2)
-        self.assertEqual(k, 4, 'The numbers are not multiplying properly')
+        self.assertEqual(k, 4, 'The numbers are not adding properly')
