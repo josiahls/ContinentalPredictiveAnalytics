4b5e6268a955313e64ff7aae266353d48f3a36d9
Josiah Laivins <jokellum@northstate.net>
2018-02-09 17:39:08 -0500

    Implmented continuous integration

--- unit_tests/ObjectTest.py
+++ unit_tests/ObjectTest.py
@@ -1,12 +1,14 @@
 import unittest
 
+
 class ObjectTest(unittest.TestCase):
     def test_object_test_adder(self):
+        print("Startinging")
         from ObjectForTesting import ObjectForTesting
         m = ObjectForTesting()
-        k = m.adder(1,1)
-        self.assertEquals(k, 1, 'The numbers are not multiplying properly')
+        k = m.adder(1, 1)
+        self.assertEqual(k, 1, 'The numbers are not multiplying properly')
         k = m.adder(1, 2)
-        self.assertEquals(k, 3, 'The numbers are not multiplying properly')
+        self.assertEqual(k, 3, 'The numbers are not multiplying properly')
         k = m.adder(2, 2)
-        self.assertEquals(k, 4, 'The numbers are not multiplying properly')
+        self.assertEqual(k, 4, 'The numbers are not multiplying properly')
---
+++ unit_tests/__init__.py
@@ -0,0 +1,1 @@
+from .ObjectTest import ObjectTest
