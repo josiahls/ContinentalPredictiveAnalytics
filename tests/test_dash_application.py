7d5f0f825884b259be42622c6584fde6336c9385
Josiah Laivins <jokellum@northstate.net>
2018-02-16 23:00:09 -0500

    Install pip install dash-core-components==0.13.0-rc4 for tabs to be added.
    I updated the Utility object.
    
    I get multiple tabs and independent UIs showing
    
    I have 2 test modules for you to reference. You should not have to touch the app file now to show your stuff

--- tests/test_dash_application.py
+++ tests/test_dash_application.py
@@ -3,6 +3,5 @@ import unittest
 
 class test_dash_application(unittest.TestCase):
     def test_load_modules(self):
-        import dash_html_components as html
         import app as a
         a.load_modules()
