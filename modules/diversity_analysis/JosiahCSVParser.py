d36cf6c0e419d6c6118941f8b04d9a4f02b6f975
josiahls <josiahls@users.noreply.github.com>
2018-02-22 18:30:47 -0500

    CSV script now loads and removes unneeded columns

--- modules/diversity_analysis/JosiahCSVParser.py
+++ modules/diversity_analysis/JosiahCSVParser.py
@@ -1,3 +1,5 @@
+import typing
+
 import pandas as pd
 import os
 from util.utility import Utility
@@ -25,12 +27,45 @@ class JosiahCSVParser(object):
                               'UNCC_HR Master Data active employees.xlsx',
                               'UNCC My Success.csv',
                               'MFG10YearTerminationData.csv']
+
+        self.desired_columns = {'UNCC_Termination pre 2017': [
+            'Personnel No.',
+            'Job',
+            'Employee Group',
+            'Employee Subgroup',
+            'Gender Key',
+            'Ethnicity',
+            'Termination',
+            'Reason for action'
+        ], 'UNCC_HR Master Data active employees': [
+            'Personal No.',
+            'Job',
+            'Position',
+            'Employee Group',
+            'Employee Subgroup',
+            'Gender Key',
+            'Pay scale type',
+        ], 'UNCC My Success': [
+            'Employee Id',
+            'Gender',
+            'Nationality',
+            'Employee Group',
+            'Employee Subgroup',
+            'Position Title'
+        ], 'MFG10YearTerminationData': [
+            'EmployeeID',
+            'city_name',
+            'job_title',
+            'gender_short'
+        ]}
+
         self.master_csv = {}
 
     def load_CSVs(self, read_limit=None):
         for location in self.csv_locations:
             if location.__contains__('.xlsx'):
                 data = pd.read_excel(self.data_workspace + location).iloc[:read_limit]
+                new_data = pd.DataFrame(data=data)
                 self.master_csv[location.split('.xlsx')[0]] = data
             if location.__contains__('.csv'):
                 data = pd.read_csv(self.data_workspace + location, nrows=read_limit)
@@ -38,18 +73,23 @@ class JosiahCSVParser(object):
 
         for key in self.master_csv:
             ut.context("Loaded data sheets: " + key)
+            # Drop Columns that are not important
 
-    def generate_CSV(self):
+    def generate_CSV(self, show_limit=5):
+        data_frame = pd.DataFrame(self.master_csv['UNCC_Termination pre 2017'])
+        # ut.context("Showing dataframe before editing \n" + data_frame.head(show_limit).to_string())
+        # Drop columns that we dont want
+        ut.context(str(len(data_frame)))
+        for key in data_frame:
+            if key not in self.desired_columns['UNCC_Termination pre 2017']:
+                data_frame.drop(labels=key, axis=1, inplace=True)
+        # ut.context("Showing dataframe after key drop \n" + data_frame.head(show_limit).to_string())
 
-        for key in self.master_csv['UNCC_Termination pre 2017']:
-            ut.context(key)
-
-
-
-        pass
-
+        # Clean data
+        for key in data_frame:
+            ut.context("Show value occupancies: " + key + ": " + str(data_frame[key].unique()))
 
 if __name__ == '__main__':
     dp = JosiahCSVParser()
-    dp.load_CSVs(read_limit=10)
-    dp.generate_CSV()
+    dp.load_CSVs(read_limit=None)
+    dp.generate_CSV(300)
